f6f3a38ddc2824d0bc7f77b9d919b2c1d16f99c9
Your Name <kkipaper@163.com>
2025-02-12 22:21:40 +0800

    update ch3 code

---
+++ book1/code/ch3.go
@@ -0,0 +1,19 @@
+//package main
+//
+//func main() {
+//	var v1, v2 int
+//	v3, v4 := f1(v1, v2)
+//	println(&v1, &v2, &v3, &v4)
+//	f2(v3)
+//
+//}
+//
+//func f1(a1, a2 int) (r1, r2 int) {
+//	var l1, l2 int
+//	println(&r2, &r1, &a2, &a1, &l1, &l2)
+//	return
+//}
+//
+//func f2(a1 int) {
+//	println(&a1)
+//}

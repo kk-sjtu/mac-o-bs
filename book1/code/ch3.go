a2a33c2ff44e6551134d95246c2823b746138f45
Your Name <kkipaper@163.com>
2025-02-13 19:20:55 +0800

    update ch3 函数栈帧与逃逸分析的内容。

--- book1/code/ch3.go
+++ book1/code/ch3.go
@@ -1,5 +1,6 @@
-//package main
-//
+package main
+
+// 3.1.1
 //func main() {
 //	var v1, v2 int
 //	v3, v4 := f1(v1, v2)
@@ -13,7 +14,30 @@
 //	println(&r2, &r1, &a2, &a1, &l1, &l2)
 //	return
 //}
-//
+
 //func f2(a1 int) {
 //	println(&a1)
 //}
+// 3.1.3
+
+type S struct {
+	a int8
+	b int64
+	c int32
+	d int16
+}
+
+func f1(s S) (r S) {
+	println(&r.a, &r.b, &r.c, &r.d, &s.a, &s.b, &s.c, &s.d)
+	return s
+}
+
+func f2(aa int8, bb int64, cc int32, dd int16) (ra int8, rb int64, rc int32, rd int16) {
+	println(&ra, &rb, &rc, &rd, &aa, &bb, &cc, &dd)
+	return
+}
+
+func main() {
+	f1(S{})
+	f2(0, 0, 0, 0)
+}

1a4438c8a30da1c3caf0e1e9964f4cce41a33ad9
Your Name <kkipaper@163.com>
2025-02-15 22:20:46 +0800

    ch4_poW algo

---
+++ book2/code/ch4_proofOfWork/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"time"
+)
+
+func main() {
+	pow("Tanmay Bakshi + Baheer Kamal", 24)
+}
+
+func pow(prefix string, bitLength int) {
+	start := time.Now()
+
+	totalHashesProcessed := 0
+	seed := uint64(time.Now().Local().UnixNano())
+	randomBytes := make([]byte, 20)
+	randomBytes = append([]byte(prefix), randomBytes...)
+	for {
+		totalHashesProcessed++
+		seed = RandomString(randomBytes, len(prefix), seed)
+		if Hash(randomBytes, bitLength) {
+			fmt.Println(string(randomBytes))
+			break
+		}
+	}
+	end := time.Now()
+
+	fmt.Println("time:", end.Sub(start).Seconds())
+	fmt.Println("process", humanize.Comma(int64(totalHashesProcessed)), "hashes")
+	fmt.Printf("hash rate: %s hashes per second\n", humanize.Comma(int64(float64(totalHashesProcessed)/end.Sub(start).Seconds())))
+}
+
+func Hash(data []byte, bitLength int) bool {
+	for i := 0; i < bitLength; i++ {
+		if data[i/8]&(1<<uint(i%8)) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+var charactes = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func RandomNumber(seed uint64) uint64 {
+	seed ^= seed << 21
+	seed ^= seed >> 35
+	seed ^= seed << 4
+	return seed
+}
+
+func RandomString(data []byte, length int, seed uint64) uint64 {
+	for i := 0; i < length; i++ {
+		seed = RandomNumber(seed)
+		data[i] = charactes[seed%uint64(len(charactes))]
+	}
+	return seed
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module your-module-name
 go 1.23.6
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
 	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
 	github.com/faiface/pixel v0.10.0 // indirect
